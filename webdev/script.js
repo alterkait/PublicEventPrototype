87c2dbcee13136041afc03828514a8344a6195b7
alterkait <alterkait2001@icloud.com>
2021-03-11 12:29:44 +0000

    added viewer access rights

--- webdev/script.js
+++ webdev/script.js
@@ -1,13 +1,18 @@
 //Only function here is to allow user to login with username=admin, password=password
+//for viewers its username=viewer, password=password
 
 function submitFunction(){
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
 
-    if(user == "admin" && pass == "password"){
+    if((user == "admin" && pass == "password")){
         window.location.href="success.html"
     }
-    else{
+    else if((user == "viewer" && pass == "password")){
+        window.location.href="funders.html"
+    }
+    
+    if((user!= "admin" && pass == "password") && (user!= "viewer" && pass == "password")) {
         alert("Incorrect username or password");
     }
 }
